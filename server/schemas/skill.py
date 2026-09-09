6cde056d340fc0c4c74672a726c5fbb36d893a2a
Makkovs <kovalmax09@gmail.com>
2026-09-09 13:50:15 +0200

    Fixed refresh_jobs script

--- server/schemas/skill.py
+++ server/schemas/skill.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, ConfigDict, Field, PositiveInt
 
 class SkillSchema(BaseModel):
-    id: PositiveInt | None 
+    id: PositiveInt | None = None
     name: str = Field(..., min_length=1, max_length=50)
 
     model_config = ConfigDict(from_attributes=True)
--- server/scripts/refresh_jobs.py
+++ server/scripts/refresh_jobs.py
@@ -1,53 +1,59 @@
 import os
 import sys
+import asyncio
+from sqlalchemy import delete, select
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from db import SessionLocal
+from db import AsyncSessionLocal
 from schemas import JobSchema
 from models import Job, Skill, JobSkill
 from dataset_generation.main import generate_job
 
-def clear_db(db):
-    db.query(JobSkill).delete()
-    db.query(Job).delete()
-    db.query(Skill).delete()
-    db.commit()
+async def clear_db(db):
+    await db.execute(delete(JobSkill))
+    await db.execute(delete(Job))
+    #For now we don't delete skills
+    await db.commit()
     print("Old data was deleted!")
 
-def seed_jobs(count: int = 1000, delete_data: str = "n"):
-    db = SessionLocal()
-    try: 
-        if delete_data == "y":
-            clear_db(db)
-            
-        existing_skills = {skill.name: skill for skill in db.query(Skill).all()}
+async def seed_jobs(count: int = 1000, delete_data: str = "n"):
+    async with AsyncSessionLocal() as db:
+        try: 
+            if delete_data == "y":
+                await clear_db(db)
 
-        for _ in range(count):
-            job_data = generate_job()
-            job_schema = JobSchema.model_validate(job_data)
-            new_job = Job(**job_schema.model_dump(exclude={"skills"}))
+            skills_query = await db.scalars(select(Skill))
+            existing_skills = {skill.name: skill for skill in skills_query.all()}
 
-            for skill_item in job_schema.skills:
-                skill_name = skill_item.name
-                if skill_name not in existing_skills:
-                    new_skill = Skill(name = skill_name)
-                    existing_skills[skill_name] = new_skill
+            jobs_to_add = []
 
-                skill_obj = existing_skills[skill_name]
-                job_skill = JobSkill(job=new_job, skill=skill_obj)
-                db.add(job_skill)
+            for _ in range(count):
+                job_data = generate_job()
+                job_schema = JobSchema.model_validate(job_data)
+                new_job = Job(**job_schema.model_dump(exclude={"skills"}))
 
-            db.add(new_job)
+                for skill_item in job_schema.skills:
+                    skill_name = skill_item.name
+                    if skill_name not in existing_skills:
+                        new_skill = Skill(name = skill_name)
+                        existing_skills[skill_name] = new_skill
+                        db.add(new_skill)
+                        
+                    skill_obj = existing_skills[skill_name]
+                    JobSkill(job=new_job, skill=skill_obj)
 
-        db.commit()
-        print(f"Successful added {count} jobs!")
-    except Exception as e:
-        db.rollback()
-        print(e)
-    finally:
-        db.close()
+                jobs_to_add.append(new_job)
+
+            db.add_all(jobs_to_add)
+
+            await db.commit()
+            print(f"Successful added {count} jobs!")
+
+        except Exception as e:
+            await db.rollback()
+            print(e)
 
 if __name__ == "__main__":
     delete_data = input("Delete old data? (Y/N): ").lower()
     jobs_count = int(input("Jobs Count: "))
-    seed_jobs(jobs_count, delete_data)
+    asyncio.run(seed_jobs(jobs_count, delete_data))
